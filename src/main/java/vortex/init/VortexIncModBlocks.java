32793a6f664c7a5e49e19322f1542d0cd7deaea6
mael-bm <maelb@DESKTOP-LSVT1NA>
2022-01-23 16:55:55 +0100

    test

---
+++ src/main/java/vortex/init/VortexIncModBlocks.java
@@ -0,0 +1,32 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package vortex.init;
+
+import vortex.block.TestBlockBlock;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.world.level.block.Block;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class VortexIncModBlocks {
+	private static final List<Block> REGISTRY = new ArrayList<>();
+	public static final Block TEST_BLOCK = register(new TestBlockBlock());
+
+	private static Block register(Block block) {
+		REGISTRY.add(block);
+		return block;
+	}
+
+	@SubscribeEvent
+	public static void registerBlocks(RegistryEvent.Register<Block> event) {
+		event.getRegistry().registerAll(REGISTRY.toArray(new Block[0]));
+	}
+}
